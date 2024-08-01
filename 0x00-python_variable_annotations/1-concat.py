61402b47bfa2a1d511675a785f8e4d1fc7174b9b
Oloishorua <jacoboloishorua@gmail.com>
2024-08-02 00:18:35 +0300

    takes 2 strings and concatenates to a new string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Program that takes a string str1
+and a string str2 as arguments and returns a concatenated string
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concatenate two strings"""
+    return str1 + str2
