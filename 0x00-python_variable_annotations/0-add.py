d1e89f1bd84ce9962bf407ebd007e598ccbd7a80
Oloishorua <jacoboloishorua@gmail.com>
2024-08-02 00:14:07 +0300

    adds 2 floats

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""program that takes float a and
+float b as arguments and return their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Add two numbers"""
+    return (a + b)
